5fcc1b280cd7c9cf5b01a188fe6d7c37aa6b165d
Mario Torres <scaramuzzini@gmail.com>
2024-06-07 10:30:34 -0400

    Import inicial

---
+++ app/src/main/java/com/oceanbrasil/rmdeadoralivegame/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.oceanbrasil.rmdeadoralivegame
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        val buttonPlay = findViewById<Button>(R.id.buttonPlay)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val intentGame = Intent(this@MainActivity, GameActivity::class.java)
+        buttonPlay.setOnClickListener {
+            startActivity(intentGame)
+        }
+
+    }
+}
