7e7fd8a54fd63fb6d72324851bcf77958ba0cf70
Mario Torres <scaramuzzini@gmail.com>
2024-06-07 11:25:09 -0400

    Character name implementado

--- app/src/main/java/com/oceanbrasil/rmdeadoralivegame/GameActivity.kt
+++ app/src/main/java/com/oceanbrasil/rmdeadoralivegame/GameActivity.kt
@@ -1,21 +1,36 @@
 package com.oceanbrasil.rmdeadoralivegame
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class GameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_game)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        val textName = findViewById<TextView>(R.id.textViewName)
+
+        RetrofitInstance.api.getCharacter(1).enqueue(object: Callback<Character> {
+            override fun onResponse(call: Call<Character>, response: Response<Character>) {
+                val character = response.body()
+                character?.let {
+                    Log.d("Resposta", it.name)
+                    textName.text = it.name
+                }
+            }
+
+            override fun onFailure(call: Call<Character>, t: Throwable) {
+                Log.e("Erro", t.message.toString())
+            }
+        })
 
 
     }
